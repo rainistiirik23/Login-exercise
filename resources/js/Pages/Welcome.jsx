e97c2d1e0aa3d2e6524c26f7940358550295957f
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-09-16 11:51:41 +0300

    First commit

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,10 @@
+import { Head } from '@inertiajs/react'
+export default function Welcome() {
+    return (
+      <div>
+        <Head title="Welcome" />
+        <h1>Welcome</h1>
+        <p>Hello, welcome to your first Inertia app!</p>
+      </div>
+    )
+  }
