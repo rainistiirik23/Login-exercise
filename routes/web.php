856201110e11e57d44a5b85d48fb01c83bb7a190
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-09-19 12:46:40 +0300

    Added login roue

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+
 Route::get('/', function () {
     return Inertia::render('Welcome');
-});
+})->name('index');
+Route::post('/logIn', [UsersController::class, 'verifyUser'])->name('login');
